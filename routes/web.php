d0708ee128788d798fcceaeb1b276457475f26c2
vagrant <vagrant@vagrant.vm>
2020-09-24 00:19:47 +0000

    add layout

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'welcome');
+
 // Route::get('/logout', 'Auth\LoginController@logout');
 // Route::get('/logout', 'Auth\AuthController@logout');
 
